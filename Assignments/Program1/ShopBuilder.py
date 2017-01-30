14d21c61cc1b162d389d9a459ebb3e94c3dfbc44
coursework <coursework@coursework.csc.tntech.edu>
2017-01-30 02:25:50 -0600

    Debugged and fixed insert code. Insert code was not committing inserted items into the database.

--- Assignments/Program1/ShopBuilder.py
+++ Assignments/Program1/ShopBuilder.py
@@ -18,17 +18,17 @@ Create user defined tables if it does not exist
 '''
 def create_table(cursor, table_nm, fields):
 	command = "CREATE TABLE IF NOT EXISTS " + table_nm  + " " + fields + " ENGINE=InnoDB"
-	print command
 	cursor.execute(command)
 
 
 '''
 Insert items to user defined tables
 '''
-def insert_items(cursor, table_nm, fields, values):
-	command = "INSERT INTO " + table_nm + " " + fields + " VALUES " + values
-	print command
-	cursor.execute(command)
+def insert_items(con, cursor, table_nm, fields, types, values):
+	command = "INSERT INTO " + table_nm + " " + fields + " VALUES " + types
+	print command, values
+	cursor.execute(command, values)
+	con.commit()
 
 
 '''
@@ -52,13 +52,46 @@ except mysql.connector.Error as err:
 		con.database = db
 
 create_table(cursor, "`UserAccounts`", "(`first_name` varchar(14) NOT NULL, `last_name` varchar(16) NOT NULL,"
-		     "`user_id` varchar (15) NOT NULL, `password` varchar(16) NOT NULL, `email` varchar(50) NOT NULL)")
+		     "`user_id` varchar (15) NOT NULL, `password` varchar(16) NOT NULL, `email` varchar(50) NOT NULL, PRIMARY KEY(user_id))")
 
 create_table(cursor, "`Inventory`", "(`prod_id` varchar(10) NOT NULL, `prod_name` varchar(20) NOT NULL,`description`"
-		     "varchar(50), `price` decimal(9,2) NOT NULL, `in_cart` int NOT NULL DEFAULT 0)")
+		     "varchar(50), `price` decimal(9,2) NOT NULL, `in_cart` int NOT NULL DEFAULT 0, PRIMARY KEY(prod_id))")
+
+#insert_st = ("INSERT INTO UserAccounts (first_name, last_name, user_id, password, email) VALUES (%s,%s,%s,%s,%s)")
+#data = ('Darren', 'Cunningham', '123dr', 'oassw','awd@gmai.com')
+#cursor.execute(insert_st, data)
+#con.commit()
+
+insert_items(con, cursor, "UserAccounts", "(first_name, last_name, user_id, password, email)", "(%s,%s,%s,%s,%s)", ('Darren', 'Cunningham'
+			  , '1232ad', 'pass', 'ois@gmail.com'))
 		
-insert_items(cursor, "`Inventory`","(`prod_id`,`prod_name`,`description`,`price`,`in_cart`)","('1239239CR','ServerT','Server Tower'"
-		     ", 10.95,'')")
+insert_items(con, cursor, "Inventory","(prod_id, prod_name, description, price, in_cart)","(%s,%s,%s,%s,%s)",('1239239CR', 'ServerT', 'Server Tower'
+		     , '10.95', ''))
+
+insert_items(con, cursor, "`Inventory`","(`prod_id`,`prod_name`,`description`,`price`,`in_cart`)","(%s,%s,%s,%s,%s)",('12459JR','Router','A Mega Awesome Router'
+		     , 2000.00,''))
+
+insert_items(con, cursor, "`Inventory`","(`prod_id`,`prod_name`,`description`,`price`,`in_cart`)","(%s,%s,%s,%s,%s)",('87890CR','Firewall',
+			  'The most epic Firewall you will ever buy', 10000.00,''))
+
+insert_items(con, cursor, "`Inventory`","(`prod_id`,`prod_name`,`description`,`price`,`in_cart`)","(%s,%s,%s,%s,%s)",('KO2304','IDS/IPS',"'Intrusion" 
+		     "Detection System/Intrusion Prevention System'", 980.00,''))
+
+insert_items(con, cursor, "`Inventory`","(`prod_id`,`prod_name`,`description`,`price`,`in_cart`)","(%s,%s,%s,%s,%s)",('99233KOP','VPN','Virtual Private Network'
+		     , 5.99,''))
+
+insert_items(con, cursor, "`Inventory`","(`prod_id`,`prod_name`,`description`,`price`,`in_cart`)","(%s,%s,%s,%s,%s)",('KIJ232103','Ethernet Cord','Cat 5 Ethernet Cord'
+		     , 2.99,''))
+
+insert_items(con, cursor, "`Inventory`","(`prod_id`,`prod_name`,`description`,`price`,`in_cart`)","(%s,%s,%s,%s,%s)",('OKR12321','AC Plug','Server power cord'
+		     , 0.99,''))
+
+insert_items(con, cursor, "`Inventory`","(`prod_id`,`prod_name`,`description`,`price`,`in_cart`)","(%s,%s,%s,%s,%s)",('CH1203','Snickers Bar',
+			  'Gotta keep those muscles growing...', 99.99,''))
+
+insert_items(con, cursor, "`Inventory`","(`prod_id`,`prod_name`,`description`,`price`,`in_cart`)","(%s,%s,%s,%s,%s)",('C12345R','Burgers',
+			  'Need something random here....', 5.45,''))
+
 cursor.execute("select * from Inventory")
 
 result = cursor.fetchall()
