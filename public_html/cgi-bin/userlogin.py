1651e9ab4e3ffb347f1ed726bae92efbbcf1ecbf
coursework <coursework@coursework.csc.tntech.edu>
2017-01-30 15:05:13 -0600

    Initial Shopper Login Screen

---
+++ public_html/cgi-bin/userlogin.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import os
+import sys
+
+sys.path.append('/home/coursework/Assignments/Program1')
+import ShopBuilder
+
+print "Content-type:text/html\r\n\r\n"
+print "<html>"
+print "<head>"
+print "<title>Shopper Login Screen</title>"
+print "</head>"
+
+print "<body>"
+print "<form>"
+print "User ID:<input type='text' id='userid'><br>"
+print "</form>"
+print "</body>"
+print "</html>"
