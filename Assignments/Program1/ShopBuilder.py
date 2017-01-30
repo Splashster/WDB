0a8cf400bf20e313e9b5e7a1e530e6261c35f42e
coursework <coursework@coursework.csc.tntech.edu>
2017-01-30 00:31:47 -0600

    Added coded to insert items to table

--- Assignments/Program1/ShopBuilder.py
+++ Assignments/Program1/ShopBuilder.py
@@ -14,12 +14,22 @@ def create_db(cursor):
 
 
 '''
-Create shop inventory table if it does not exist
+Create user defined tables if it does not exist
 '''
 def create_table(cursor, table_nm, fields):
 	command = "CREATE TABLE IF NOT EXISTS " + table_nm  + " " + fields + " ENGINE=InnoDB"
 	print command
 	cursor.execute(command)
+
+
+'''
+Insert items to user defined tables
+'''
+def insert_items(cursor, table_nm, fields, values):
+	command = "INSERT INTO " + table_nm + " " + fields + " VALUES " + values
+	print command
+	cursor.execute(command)
+
 
 '''
 Try to connect to the database.
@@ -47,8 +57,9 @@ create_table(cursor, "`UserAccounts`", "(`first_name` varchar(14) NOT NULL, `las
 create_table(cursor, "`Inventory`", "(`prod_id` varchar(10) NOT NULL, `prod_name` varchar(20) NOT NULL,`description`"
 		     "varchar(50), `price` decimal(9,2) NOT NULL, `in_cart` int NOT NULL DEFAULT 0)")
 		
-
-cursor.execute("SHOW TABLES")
+insert_items(cursor, "`Inventory`","(`prod_id`,`prod_name`,`description`,`price`,`in_cart`)","('1239239CR','ServerT','Server Tower'"
+		     ", 10.95,'')")
+cursor.execute("select * from Inventory")
 
 result = cursor.fetchall()
 
