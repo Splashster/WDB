6283f819d14b7b2de83a3297f09fbda1b3ca40c7
coursework <coursework@coursework.csc.tntech.edu>
2017-01-30 18:41:35 -0600

    Moved password verification into registration page

--- public_html/cgi-bin/pass_verification.py
+++ public_html/cgi-bin/pass_verification.py
@@ -18,3 +18,14 @@ if passwd == conf_passwd:
 	print passwd, conf_passwd
 	print "</body>"
 	print "</html>"
+else:
+	print "Content-type:text/html \r\n\r\n"
+	print "<html>"
+	print "<head></head>"
+	print "<body>"
+	print passwd, conf_passwd
+	print "<script type=text/javascript>"
+	print "alert('Incorrect Password!')"
+	print "</script>"
+	print "</body>"
+	print "</html>"
