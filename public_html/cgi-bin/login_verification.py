d26d4f00ecac824a0da0ff5d130083117cf121a1
coursework <coursework@coursework.csc.tntech.edu>
2017-02-02 01:50:41 -0600

    Added login functionality. User Authentication has been added as well.

---
+++ public_html/cgi-bin/login_verification.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+import cgi
+import os
+import sys
+import mysql.connector
+
+sys.path.append('/home/coursework/Assignments/Program1/')
+
+from Database import *
+
+con = mysql.connector.connect(user=user, password=passwd, host=host, database=db)
+cursor = con.cursor(buffered=True)
+
+form_items = cgi.FieldStorage()
+useriden = form_items.getvalue('userident')
+user_pass = form_items.getvalue('user_passwd')
+
+command = "SELECT * FROM `UserAccounts` where id=%s and password=%s"
+cursor.execute(command, (useriden, user_pass))
+result = cursor.fetchall()
+if cursor.rowcount == 1:
+	print """Content-type:text/html\r\n\r\n
+	  	<html>
+		<body>
+		<script type='text/javascript'>
+		alert('Login Successful!')
+		</script>
+		</body>
+		</html>"""
+else:
+	print """Content-type:text/html\r\n\r\n
+		<html>
+		<body>
+		<script type='text/javascript'>
+		alert('Username/Password is Incorrect!')
+		</script>
+		</body>
+		</html>"""
+
+cursor.close()
+con.close()
--- public_html/cgi-bin/userlogin.py
+++ public_html/cgi-bin/userlogin.py
@@ -2,23 +2,26 @@
 
 import os
 import sys
+import cgi
 
 sys.path.append('/home/coursework/Assignments/Program1')
 import ShopBuilder
 
-print "Content-type:text/html\r\n\r\n"
-print "<html>"
-print "<head>"
-print "<title>Shopper Login Screen</title>"
-print "</head>"
+print """Content-type:text/html\r\n\r\n
+<html>
+<head>
+<title>Shopper Login Screen</title>
+</head>
 
-print "<body>"
-print "<h1>Please log in</h1><br>"
-print "<form>"
-print "User ID:<input type='text' id='userid'><br>"
-print "Password:<input type='password' id='passwd'><br>"
-print "<input type='submit' value='Login'><br><br>"
-print "<p><a href='http://localhost/~coursework/cgi-bin/registration.py'>Register as a new user</a></p>"
-print "</form>"
-print "</body>"
-print "</html>"
+<body>
+<h1>Please log in</h1><br>
+<form name='login' action='http://localhost/~coursework/cgi-bin/login_verification.py?userid=123&passwd=123' method='GET'>
+User ID:<input type='text' name='userident' value=""  required><br>
+Password:<input type='password' name='user_passwd' value="" required><br>
+<input type='submit' value='Login'><br><br>
+<p><a href='http://localhost/~coursework/cgi-bin/registration.py'>Register as a new user</a></p>
+</form>
+</body>
+</html>"""
+
+
