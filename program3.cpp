6e7c8f1e77f4e95ae87024ba46bd7db397bd3a23
Splashster <psotails@gmail.com>
2017-04-13 17:33:27 -0500

    Update program3.cpp
    Generating initial UVs

--- program3.cpp
+++ program3.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <sstream>
 #include <stdlib.h>
+#include <math.h>
 
 using namespace std;
 
@@ -12,10 +13,18 @@ struct Records {
 	string time;
 	string file_request;
 	int size;
-	int uv_score;
+	int frequency_requested;
+	float uv_score;
 };
 
 Records *record;
+float epa_bw;
+float epa_cd;
+float clark_bw;
+float clark_cd;
+float wb;
+float wf;
+int cache_size;
 
 int getLineCount (string filename){
 	string line;
@@ -60,6 +69,7 @@ void storeRecords(string filename) {
 			record[num].time = tokens[2];
 			record[num].file_request = tokens[3];
 			record[num].size = atoi(tokens[4].c_str());
+			record[num].frequency_requested = 1;
 			num++;
 			
 		}
@@ -67,46 +77,54 @@ void storeRecords(string filename) {
 	}
 	file.close();
 //	cout << tokens[0] << " " << record[0].server << endl;
-	cout << "Record 4 size: " << record[3].size << endl;
-	cout << "Record 5 file requested: " << record[4].file_request << endl;
+	//cout << "Record 4 size: " << record[3].size << endl;
+	//cout << "Record 5 file requested: " << record[4].file_request << endl;
 }
 
-//void generateUVScores(){
-//	for(int i = 0; i < ]
-//}
+float generateUVScore(string server, int frequency_req, int size){
+	float score = 0.0;
+	if(server == "clark"){
+		//cout << server << " " << frequency_req << " " << size << endl;
+		score = ((clark_cd+(wb/clark_bw))*(pow(frequency_req,wf)))/size;
+		//cout << score << endl;
+	}else{
+		score = ((clark_cd+(wb/clark_bw))*pow(frequency_req,wf))/size;
+	}
+	return score;
+}
+
+void cacheStoring(int i, string cache[]){
+	record[i].uv_score = generateUVScore(record[i].server, record[i].frequency_requested, record[i].size);
+	
+}
 
 int main(int argc, char *argv[]){
-	int epa_bw;
-	int epa_cd;
-	int clark_bw;
-	int clark_cd;
-	int wb;
-	int wf;
-	int cache_size;
 	string filename;
+	//set frequency back to 1
 	int num_lines = 0;
 	
 	filename = argv[1];
-
 	num_lines = getLineCount(filename);
-
 	record = new Records[num_lines];
-	//epa_bw = strtol(argv[2], NULL, 10);
-	//epa_cd = strtol(argv[3], NULL, 10);
-	//clark_bw = strtol(argv[4], NULL, 10);
-	//clark_cd = strtol(argv[5], NULL, 10);
-	//wb = strtol(argv[6], NULL, 10);
-	//wf = strtol(argv[7], NULL, 10);
-	//cache_size = strtol(argv[8], NULL, 10);
-	storeRecords(filename);
-	//generateUVScores();
-	//string clark_cache[cache_size];
-	//string epa_cache[cache_size];
-	
-	record[5].server = "Done";	
+	epa_bw = stof(argv[2]);
+	epa_cd = stof(argv[3]);
+	clark_bw = stof(argv[4]);
+	clark_cd = stof(argv[5]);
+	wb = stof(argv[6]);
+	wf = stof(argv[7]);
+	cache_size = strtol(argv[8], NULL, 10);
+	string cache[num_lines];
 
-	cout << "num_lines: " << num_lines << endl;
-	cout << "Item: " << record[5].server << endl;
+	storeRecords(filename);
+	for(int i = 0; i < num_lines; i++){
+		cacheStoring(i, cache);
+		cout << "UV Score: " << record[i].uv_score << endl;
+		
+	}
+	
+	
+	//cout << "num_lines: " << num_lines << endl;
+	
 
 	delete[] record;
 	
